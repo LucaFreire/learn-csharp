93f2f382df6b2f5807e7f97910e94780979276db
LucaFreire <lucasrodifreire@gmail.com>
2022-10-25 09:44:32 -0300

    Arvore

--- Aula6/Program.cs
+++ Aula6/Program.cs
@@ -15,78 +15,78 @@ using System;
 // }
 
 
-public class LinkedList<T>
-{
-    private LinkedListNode<T> first = null;
-    private int count = 0;
-    public int Count => count;
-    public T this[int index]
-    {
-        get
-        {
-            if(first == null)
-                throw new IndexOutOfRangeException();
+// public class LinkedList<T>
+// {
+//     private LinkedListNode<T> first = null;
+//     private int count = 0;
+//     public int Count => count;
+//     public T this[int index]
+//     {
+//         get
+//         {
+//             if(first == null)
+//                 throw new IndexOutOfRangeException();
 
-            var atual = first;
-            for (int i = 0; i < index; i++)
-            {
-                if (atual.Next == null)
-                    throw new IndexOutOfRangeException();
+//             var atual = first;
+//             for (int i = 0; i < index; i++)
+//             {
+//                 if (atual.Next == null)
+//                     throw new IndexOutOfRangeException();
                 
-                atual = atual.Next;
-            }
-            return atual.Value;
-        }
-        set
-        {
-            if(first == null)
-                throw new IndexOutOfRangeException();
+//                 atual = atual.Next;
+//             }
+//             return atual.Value;
+//         }
+//         set
+//         {
+//             if(first == null)
+//                 throw new IndexOutOfRangeException();
 
-            var atual = first;
-            for (int i = 0; i < index; i++)
-            {
-                if (atual.Next == null)
-                    throw new IndexOutOfRangeException();
+//             var atual = first;
+//             for (int i = 0; i < index; i++)
+//             {
+//                 if (atual.Next == null)
+//                     throw new IndexOutOfRangeException();
                 
-                atual = atual.Next;
-            }
+//                 atual = atual.Next;
+//             }
 
-            atual.Value = value;
-        }
-    }
+//             atual.Value = value;
+//         }
+//     }
 
-    public void Add(T value)
-    {
-        if (first == null)
-        {
-            first = new LinkedListNode<T>(value);
-            first.Value = value;
-            count++;
-            return;
-        }
-        var atual = first;
-        while(atual.Next != null)
-            atual = atual.Next;
+//     public void Add(T value)
+//     {
+//         if (first == null)
+//         {
+//             first = new LinkedListNode<T>(value);
+//             first.Value = value;
+//             count++;
+//             return;
+//         }
+//         var atual = first;
+//         while(atual.Next != null)
+//             atual = atual.Next;
         
 
-        atual.Next = new LinkedListNode<T>();
-        atual.Next.Value = value;
-        count++;
-    }
-}
+//         atual.Next = new LinkedListNode<T>();
+//         atual.Next.Value = value;
+//         count++;
+//     }
+// }
 
-public class LinkedListNode<T>
-{
-    public T Value { get; set; }
-    public LinkedListNode<T> Next { get; set; }
+// public class LinkedListNode<T>
+// {
+//     public T Value { get; set; }
+//     public LinkedListNode<T> Next { get; set; }
 
-    public LinkedListNode(){}
+//     public LinkedListNode(){}
 
-    public LinkedListNode(T value)
-    {
-        this.Value = value;
-    }
-}
+//     public LinkedListNode(T value)
+//     {
+//         this.Value = value;
+//     }
+// }
 
 
 // double num = 25;
@@ -125,4 +125,3 @@ public static class MyExtensionsMethods{
     }
     
 }
-
--- Aula7/Program.cs
+++ Aula7/Program.cs
@@ -1,1 +1,102 @@
-﻿// Programa da Árvore
+﻿// // Programa da Árvore
+// // Implementar estrutura de árvore binária
+
+
+// // Classe chamado Arvore com variaveis "Esquerda" e "Direita"
+// // Filho da Esquerda <= Mãe
+// // Filho da Direita >= Mãe
+// // Conter .Add(int i), bool .Contains(int i)
+
+// BinaryTree<string> bt = new BinaryTree<string>();
+// bt.Add("20f");
+// bt.Add("30f");
+// bt.Add("19f");
+// bt.Add("25f");
+
+// Console.WriteLine(bt.Contains("20f"));
+
+// public class BinaryTree<T>
+//     where T : IComparable<T>
+// {
+//     public T? Value { get; set; }
+//     public BinaryTree<T>? Left { get; set; }  = null;
+//     public BinaryTree<T>? Right { get; set; } = null;
+
+//     public void Add(T i)
+//     {
+//         Console.WriteLine("Teste");
+//         if ((this.Value.CompareTo(default(T)) == 0) || (this.Value == null))
+//         {
+//             this.Value = i;
+//         }
+//         else if (i.CompareTo(this.Value) < 0)
+//         {
+//             if (Left == null)
+//                 Left = new BinaryTree<T>();
+//             Left.Add(i);
+//         }
+//         else
+//         {  
+//             if (Right == null)
+//                 Right = new BinaryTree<T>();
+//             Right.Add(i);
+//         }
+//     }
+
+//     public bool Contains(T value)
+//     {
+//         if (value.CompareTo(this.Value) == 0)
+//             return true;
+//         if (value.CompareTo(this.Value) > 0)
+//             return Right?.Contains(value) ?? false;
+//         return Left?.Contains(value) ?? false;
+//     }
+// }
+
+
+ArvoreBinaria bt = new ArvoreBinaria();
+
+bt.Add(20);
+bt.Add(13);
+bt.Add(31);
+bt.Add(44);
+bt.Add(23);
+bt.Add(2);
+bt.Add(22);
+Console.WriteLine(bt.Contains(10));
+
+public class ArvoreBinaria
+{
+    public int? Value { get; set; }=null;
+    public ArvoreBinaria? Esquerda { get; set; } = null;
+    public ArvoreBinaria? Direita { get; set; } = null;
+
+    public void Add(int value)
+    {
+        if (this.Value == null){
+            this.Value = value;
+            this.Esquerda = new ArvoreBinaria();
+            this.Direita = new ArvoreBinaria();
+        }
+
+        else if (this.Value <= value)
+            this.Esquerda.Add(value);
+
+        else
+            this.Direita.Add(value);
+    }
+
+    public bool Contains(int value)
+    {   
+        // Entra um valor e verifica se ele existe na árvore e retorna o resultado ( True / False)
+        if (value == this.Value)
+            return true;
+        
+        else if (value < this.Value)
+            return this.Esquerda.Contains(value);
+        
+        else 
+            return this.Direita.Contains(value);
+
+    }
+}
