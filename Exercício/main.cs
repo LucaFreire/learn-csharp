257af06db2c3a0840908b321ad5d14df8dc947f4
LucaFreire <lucasrodifreire@gmail.com>
2022-10-25 11:49:41 -0300

    Exercício

---
+++ Exercício/main.cs
@@ -0,0 +1,58 @@
+using System;
+
+// Curso
+var cursos = new List<Curso>();
+
+// Aluno
+var Alunos = new List<Aluno>();
+
+while (true){
+    Console.Write("1- Cadastrar Curso\n2- Listar Cursos\n3- Cadastrar Alunos\n 4- Dar Notas\n 5- Estatística\n 6- Sair\nSeu Número: ");
+    int Num = Convert.ToInt32(Console.ReadLine());
+
+    if (Num == 1){
+
+        
+         
+
+    }
+   
+
+    // else if (Num == 2)
+    //     for (int i = 0; i<=ListaNome.Count; i++)
+    //         Console.WriteLine($"Curso: {ListaNome[i]}\nCódigo do Curso: {ListaCod[i]}\nCarga Horária: {ListaHoras[i]}\nNota do Curso: {ListaNota[i]}");
+        
+
+    // else if (Num == 3){
+    //     Console.Write("Nome do Aluno: ");
+    //     string NomeDoAluno = Console.ReadLine();
+
+    //     Console.Write("Código de Curso: ");
+    //     int CodDoAluno = Console.ReadLine();
+
+    //     Console.Write("Número de Matrícula: ");
+    //     string MatriculaDoAluno = Console.ReadLine();
+
+    //     Curso.Add2(CodDoAluno,NomeDoAluno,MatriculaDoAluno);
+    // }
+
+
+    // else if (Num == 4)
+    //     Console.WriteLine("r");
+
+    // else if (Num == 5)
+    //     Console.WriteLine("r");
+
+    // else if (Num == 6)
+    //     Console.WriteLine("r");
+    
+    else
+        Console.WriteLine("Número Inváido, Digite Novamente!");
+
+        
+}
+
+
+
+
+
