1921ff0fc1b7969b1786008bb34dcc0e8f0b78c2
LucaFreire <lucasrodifreire@gmail.com>
2022-11-09 11:51:39 -0300

    Terminar * do 10

---
+++ ExercíciosPoo/Exercício10/Program.cs
@@ -0,0 +1,40 @@
+﻿int h = 3;
+int l = 3;
+Matriz mat = new Matriz(h,l);
+
+mat.Add(0,0,25);
+mat.Add(0,1,0);
+mat.Add(0,2,0);
+
+mat.Add(1,0,0);
+mat.Add(1,1,66);
+mat.Add(1,2,0);
+
+mat.Add(2,0,0);
+mat.Add(2,1,0);
+mat.Add(2,2,45);
+
+
+
+Matriz mat2 = new Matriz(h,l);
+
+mat2.Add(0,0,25);
+mat2.Add(0,1,0);
+mat2.Add(0,2,0);
+
+mat2.Add(1,0,0);
+mat2.Add(1,1,66);
+mat2.Add(1,2,0);
+
+mat2.Add(2,0,0);
+mat2.Add(2,1,0);
+mat2.Add(2,2,45);
+
+
+// Console.WriteLine(mat.Identidade());
+// Console.WriteLine(mat.Diagonal());
+// Console.WriteLine(mat.Singular());
+// Console.WriteLine(mat.Simetrica());
+Console.WriteLine(mat.Simetrica());
+
+Console.WriteLine(mat + mat2);
