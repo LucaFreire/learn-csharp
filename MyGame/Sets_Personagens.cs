a663fb8e4a8ac758b50e5e3c1e14bf0b40535730
LucaFreire <lucasrodifreire@gmail.com>
2022-11-15 16:06:11 -0300

    15/11

--- MyGame/Sets_Personagens.cs
+++ MyGame/Sets_Personagens.cs
@@ -1,34 +1,38 @@
-public abstract class Personagens{
-    public string Nome{get;set;}
-    protected int Vida{get;set;}
-    protected int Dano{get;set;}
-    protected int Velocidade{get;set;}
-    public void Attck(class Personagens)
+public abstract class Personagens
+{
+    public string Nome{ get; set; }
+    protected int Vida{ get; set; }
+    protected int Dano{ get; set; }
+    protected int Velocidade{ get; set; }
+    public void Attck(Personagens inimigo) => inimigo.Vida -= this.Dano;
+  
+    public class Tanque : Personagens
     {
-        this.Vida -= this.Dano;
+        public Tanque()
+        {
+            this.Vida = 150;
+            this.Dano = 75;
+            this.Velocidade = 30;
+        }
     }
-}
 
-public class Tanque : Personagens{
-    public Tanque(){
-        this.Vida = 150;
-        this.Dano = 75;
-        this.Velocidade = 30;
+    public class Veloz : Personagens
+    {
+        public Veloz()
+        {
+            this.Vida = 80;
+            this.Dano = 40;
+            this.Velocidade = 70;
+        }
     }
-}
 
-public class Veloz : Personagens{
-    public Veloz(){
-        this.Vida = 80;
-        this.Dano = 40;
-        this.Velocidade = 70;
-    }
-}
-
-public class Comum : Personagens{
-    Comum(){
-        this.Vida = 100;
-        this.Dano = 60;
-        this.Velocidade = 50;
+    public class Comum : Personagens
+    {
+        public Comum()
+        {
+            this.Vida = 100;
+            this.Dano = 60;
+            this.Velocidade = 50;
+        }
     }
 }
