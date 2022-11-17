e626669b597f662670b80508683e76aca3f73d9d
Lucas Freire <104910407+LucaFreire@users.noreply.github.com>
2022-11-17 15:49:06 -0300

    Add files via upload

---
+++ ExercíciosC#/EX2/Extension.cs
@@ -0,0 +1,25 @@
+public static class Extension
+{
+    public static double Aritmetica(this IEnumerable<double> coll) => coll.Average();
+
+    public static double Ponderada(this IEnumerable<double> coll, IEnumerable<double> ListaPond)
+    {
+        var itColl = coll.GetEnumerator();
+        var itLista = ListaPond.GetEnumerator();
+        double soma = 0; 
+
+        while(itColl.MoveNext() && itLista.MoveNext())
+            soma+= itColl.Current * itLista.Current;
+        return soma;
+    }
+
+    public static double harmônica(this IEnumerable<double> coll)
+    {
+        var it = coll.GetEnumerator();
+        double soma = 0; 
+
+        while(it.MoveNext())
+            soma += 1 / it.Current;
+        return coll.Count() / soma;
+    }
+}
