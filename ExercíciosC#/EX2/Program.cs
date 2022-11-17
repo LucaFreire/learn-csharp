d2383b8526a876094d86dd999890c733a88a64ee
Lucas Freire <104910407+LucaFreire@users.noreply.github.com>
2022-11-17 17:08:54 -0300

    Update Program.cs

--- ExercíciosC#/EX2/Program.cs
+++ ExercíciosC#/EX2/Program.cs
@@ -1,16 +1,38 @@
-﻿// 2.	Um usuário deseja um algoritmo onde possa escolher que tipo de média deseja calcular a partir de 3 notas.
-// Faça um algoritmo que leia as notas, a opção escolhida pelo usuário e calcule a média.
-// 1 - aritmética
-// 2 - ponderada (3,3,4)
-// 3 - harmônica
 
 List<double> Lista = new List<double>();
 
+List<double> ListaPonderada = new List<double>{ 3, 3, 4 };
 
-Lista.Add(2);
-Lista.Add(2);
-Lista.Add(2);
+for (int i = 3; i < 3; i++)
+{
+    Console.WriteLine($"Digite seu {i+1}° número: ");
+    double num =  double.Parse(Console.ReadLine() ?? "0");
+    Lista.Add(num);
+}
 
+// double resultado = 0;
+// switch (Console.ReadLine())
+// {
+//     case "1":
+//         resultado = Lista.Aritmetica();
+//         break;
 
+//     case "2":
+//         resultado = Lista.Ponderada(ListaPonderada);
+//         break;
 
-Lista.Aritmetica();
+//     case "3":
+//         resultado = Lista.Harmonica();
+//         break;
+    
+//     default:
+//         throw new Exception("Ai não porra");
+// }
+
+double resultado = Console.ReadLine() switch
+{
+    "1" => Lista.Aritmetica(),
+    "2" => Lista.Ponderada(ListaPonderada),
+    "3" => Lista.Harmonica(),
+    _ => throw new Exception("Ai não porra")
+};
