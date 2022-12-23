711a0ceb81fef38ce6c0b4407453dfbb5c8ad087
LucaFreire <lucasrodifreire@gmail.com>
2022-12-23 18:33:14 -0300

    gitgnore update

--- Teste/Program.cs
+++ Teste/Program.cs
@@ -1,8 +1,15 @@
-﻿using System.Linq;
-List<int> nn = new List<int>(){2,2,2,2,2};
+﻿
 
-var a = nn.Sum();
+var a = 2;
+Console.WriteLine(a);
 
-var b = nn.Average();
+var b = 3;
+Console.WriteLine(b);
 
+var c = a;
+
+a = b;
+Console.WriteLine(a);
+
+b = c;
 Console.WriteLine(b);
