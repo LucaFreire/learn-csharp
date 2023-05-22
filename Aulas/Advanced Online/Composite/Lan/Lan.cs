125783f0e730b99d7fe6f53a78e9b56e3c88c773
Lucas Freire <104910407+LucaFreire@users.noreply.github.com>
2023-05-22 16:52:39 -0300

    Add: Composite

---
+++ Aulas/Advanced Online/Composite/Lan/Lan.cs
@@ -0,0 +1,24 @@
+public class Lan : IOperationHours
+{
+    List<IOperationHours> lista = new List<IOperationHours>();
+    public Lan(string name)
+        => this.Name = name;
+    public string Name { get; set; }
+    public int Hours { get; set; }
+
+    public int GetHours()
+    {
+        int hours = 0;
+        foreach (var item in lista)
+            hours += item.GetHours();
+        
+        Console.WriteLine($"Total Hours {Name}: {hours}");
+        this.Hours = hours;
+        return hours;
+    }
+
+    public void Add(IOperationHours op)
+        => lista.Add(op);
+
+
+}
